14164dec16a082d45fc8fd8a4980ada0bdf7eeda
Jorge Dias <jorgediasdsg@gmail.com>
2020-07-10 10:52:52 -0300

    🚀[X] api context

--- frontend-web/src/pages/SignIn/index.tsx
+++ frontend-web/src/pages/SignIn/index.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import AuthContext from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -15,6 +16,10 @@ import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+
+    const { name } = useContext(AuthContext);
+
+    console.log(name);
 
     const handleSubmit = useCallback(async (data: object) => {
         try {
