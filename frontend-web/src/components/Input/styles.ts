b0d86a274edb35b21298f75c93fda9c645f4a4a8
Jorge Dias <jorgediasdsg@gmail.com>
2020-07-10 09:18:00 -0300

    🚀[X] validate - Yup - Border Error

--- frontend-web/src/components/Input/styles.ts
+++ frontend-web/src/components/Input/styles.ts
@@ -1,8 +1,10 @@
+/* eslint-disable import/prefer-default-export */
 import styled, { css } from 'styled-components';
 
 interface ContainerProps {
     isFocused: boolean;
     isFilled: boolean;
+    isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -19,18 +21,25 @@ export const Container = styled.div<ContainerProps>`
     & + div {
         margin-top: 8px;
     }
-
-    ${props => props.isFocused &&
-    css`
-        color: #ff9000;
-        border-color: #ff9000;
-    `}
+    ${props =>
+        props.isErrored &&
+        css`
+            border-color: #c53030;
+        `}
 
     ${props =>
-    props.isFilled &&
-    css`
-        color: #ff9000;
-    `}
+        props.isFocused &&
+        css`
+            color: #ff9000;
+            border-color: #ff9000;
+        `}
+
+    ${props =>
+        props.isFilled &&
+        css`
+            color: #ff9000;
+        `}
+
 
     input {
         flex: 1;
@@ -47,4 +56,12 @@ export const Container = styled.div<ContainerProps>`
         margin-right: 16px;
     }
 }
+`;
+
+export const Error = styled.div`
+    height: 20px;
+    margin-left: 16px;
+    svg {
+        margin: 0;
+    }
 `;
