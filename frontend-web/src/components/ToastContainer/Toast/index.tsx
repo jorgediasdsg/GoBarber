02fac326e46fba6e21144909999d37a118302d8c
Jorge Dias <jorgediasdsg@gmail.com>
2020-07-12 12:11:16 -0300

    🚀[X] Toast types

--- frontend-web/src/components/ToastContainer/Toast/index.tsx
+++ frontend-web/src/components/ToastContainer/Toast/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import React, { useEffect } from 'react';
+import {
+    FiAlertCircle,
+    FiCheckCircle,
+    FiInfo,
+    FiXCircle,
+} from 'react-icons/fi';
 
 import { Container } from './styles';
 
@@ -9,12 +14,27 @@ interface ToastProps {
     message: ToastMessage;
 }
 
+const icons = {
+    info: <FiInfo size={24} />,
+    error: <FiAlertCircle size={24} />,
+    success: <FiCheckCircle size={24} />,
+};
+
 const Toast: React.FC<ToastProps> = ({ message }) => {
     const { removeToast } = useToast();
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            removeToast(message.id);
+        }, 3000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [removeToast, message.id]);
+
     return (
         <Container type={message.type} hasDescription={!!message.description}>
-            <FiAlertCircle size={20} />
+            {icons[message.type || 'info']}
             <div>
                 <strong>{message.title}</strong>
                 {message.description && (
